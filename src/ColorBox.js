08a2079941c8fa252da8526f30bf331928882047
github username <github email address>
2020-04-21 08:41:38 -0400

    Done.

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -4,14 +4,15 @@ export default class ColorBox extends Component {
 
   state = {
     todos: [
-      
+
     ]
   }
 
   render() {
+    let float = Number.parseFloat(this.props.opacity)
     return (
-      <div className="color-box" style={{opacity: null /*replace null with the value*/}}>
-        {/* your conditional code here! */}
+      <div className="color-box" style={{ opacity: float }}>
+        {0.2 < float ? <ColorBox opacity={float - 0.1} /> : null}
       </div>
     )
   }
